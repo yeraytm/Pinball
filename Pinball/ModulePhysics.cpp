5bf1b8d6bf0ac1ff26539dc3285ab476a59c8ba9
Bosco Barber Esbert <boscobarberesbert@gmail.com>
2020-11-11 21:14:12 +0100

    Debug folder fix

--- Pinball/ModulePhysics.cpp
+++ Pinball/ModulePhysics.cpp
@@ -7,7 +7,7 @@
 #include "math.h"
 
 #ifdef _DEBUG
-#pragma comment( lib, "Box2D/libx86/Debug/Box2D.lib" )
+#pragma comment( lib, "Box2D/libx86/DebugX/Box2D.lib" )
 #else
 #pragma comment( lib, "Box2D/libx86/Release/Box2D.lib" )
 #endif
